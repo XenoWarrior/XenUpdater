eb5df8af1820a89611ed155a33f4b6db10f44efe
Ashley Scott <admin@projectge.com>
2019-08-12 23:50:35 +0100

    PackageManager to build a list of files to download that were updated or do not exist.

--- app/js/Lib/PackageManager.js
+++ app/js/Lib/PackageManager.js
@@ -39,14 +39,14 @@ class PackageManager {
     /**
      * GetPackage
      * Will fetch a specific package configuration from the updater service / memory.
-     * @param {string} p: the package name.
+     * @param {string} packageName: the package name.
      */
-    async getPackage(p) {
+    async getPackage(packageName) {
         try {
-            if (this.packageList.hasOwnProperty(p)) {
-                return this.packageList(p);
+            if (this.packageList.hasOwnProperty(packageName)) {
+                return this.packageList(packageName);
             } else {
-                let data = await fetch(`https://xenupdater.projectge.com/${this.serviceName}/xu/update/${p}.json`);
+                let data = await fetch(`https://xenupdater.projectge.com/${this.serviceName}/xu/update/${packageName}.json`);
                 return await data.json();
             }
 
@@ -58,71 +58,110 @@ class PackageManager {
     /**
      * GetFiles
      * Gets a list of files from a specified package.
-     * @param {string} p: the package name.
+     * @param {string} packageName: the package name.
      */
-    async getFiles(p) {
-        if (this.packageList.hasOwnProperty(p)) {
+    async getFiles(packageName) {
+        if (this.packageList.hasOwnProperty(packageName)) {
             console.log(this.packageList);
-        }
-    }
-
-    /**
-     * ScanPackage
-     * Scans a package to download / downloaded by the updater.
-     * @param {String} localData: the local config data from disk
-     */
-    async scanPackage (localData) {
-        if(localData != "") {
-            // Compare the local system with the server data.#
-            console.log(`[PackageManager] Checking updates for package ${p}`);
-        } else {
-            // Scan the local system based on the server data.
-            console.log(`[PackageManager] Starting scan for package ${p}`);
         }
     }
 
     /**
      * CheckPackage
      * Will check if the current local package matches the server package.
-     * @param {string} p: Name of the package to check.
+     * @param {string} packageName: Name of the package to check.
      * @returns {object}: Files for the package that need to be updated.
      */
-    async checkPackage(p) {
-        let file = `./app/xu/${p}.json`;
+    async checkPackage(packageName) {
+        let dir = "./app/xu/";
+        let file = packageName + ".json";
+
         let localData = "";
-        let serverData = await this.getPackage(p);
+        let serverData = JSON.stringify(await this.getPackage(packageName));
 
-        console.log(serverData);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
 
-        // Check if there is already a configuration on the disk.
-        await fs.open(file, 'r', async (err, fd) => {
-            if (err) {
-                // If the configruation does not exists, write data to disk.
-                // We will need to check if there are files currently on the disk for this package.
-                if (err.code === "ENOENT") {
-                    await fs.writeFile(file, JSON.stringify(serverData), (err) => {
-                        if (err) {
-                            throw err;
-                        }
-
-                        console.log('[PackageManager] Configuration file has been saved.');
-                    });
-                } else {
-                    throw err;
+        if (fs.existsSync(dir + file)) {
+            localData = fs.readFileSync(dir + file).toString();
+        } else {
+            fs.writeFile(dir + file, serverData, function (err) {
+                if (err) {
+                    return console.log(err);
                 }
-            } else {
-                // If the configruation does exists, read it's data.
-                await fs.readFile(file, (err, data) => {
-                    if (err) {
-                        throw err;
+                console.log("[PackageManager] The configuration file was saved!");
+            });
+        }
+
+        return this.scanPackageDir(packageName, localData, serverData);
+    }
+
+    /**
+     * ScanPackage
+     * Scans a package to download / downloaded by the updater.
+     * @param {String} packageName: the package name.
+     * @param {String} localData: the local config data from disk.
+     * @param {String} serverData: the config data from server.
+     */
+    async scanPackageDir(packageName, localData, serverData) {
+        serverData = JSON.parse(serverData);
+
+        let serverFiles = Object.keys(serverData.package_files);
+        let downloadList = [];
+
+        if (localData != "") {
+            // A build already exists on the system.
+            console.log(`[PackageManager] Build exists, checking updates for package ${packageName}`);
+
+            localData = JSON.parse(localData);
+
+            let localFiles = Object.keys(localData.package_files);
+            serverFiles.forEach(f => {
+                // If a new file, or changed file, add it to download.
+                if (!fs.existsSync(`${packageName}/${f}`) || !localFiles.hasOwnProperty(f) || serverFiles[f] != localFiles[f]) {
+                    downloadList.push(f);
+                }
+            });
+        } else {
+            console.log(`[PackageManager] Local config does not exist, checking if ${packageName} has files.`);
+
+            let dir = [];
+            try {
+                dir = this.readPackageDir(packageName);
+
+                console.log("DIR", dir);
+
+                serverFiles.forEach(f => {
+                    if (!dir.includes(f)) {
+                        downloadList.push(f);
                     }
-                    
-                    localData = data;
+                })
+            } catch (ex) {
+                console.log(`[PackageManager] No build files for ${packageName}, downloading all files.`);
+                serverFiles.forEach(f => {
+                    downloadList.push(f);
                 });
             }
+        }
 
-            
-            this.scanPackage(localData);
-        });
+        return downloadList;
     }
+
+    /**
+     * ReadPackageDir
+     * Will read all the contents of a package directory
+     * @param {string} packageName: the package name.
+     * @throws {exception}: if the directory does not exist.
+     * @returns {array}: the files in a directory.
+     */
+    readPackageDir(packageName) {
+        try {
+            let dir = fs.readdirSync(`./${packageName}`);
+            return dir;
+        } catch (ex) {
+            throw ex;
+        }
+    }
+
 }
